1f374cbbef7654b452da7b1a8dc15e0a5b097167
Jamaine <jamaine.spooner@gmail.com>
2015-04-20 12:01:58 +0100

    Create detect-ios-version.js

---
+++ detect-ios-version.js
@@ -0,0 +1,13 @@
+function iOSversion() {
+  if (/iP(hone|od|ad)/.test(navigator.platform)) {
+    // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+    var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+  }
+}
+
+ver = iOSversion();
+
+if (ver[0] >= 5) {
+  alert('This is running iOS 5 or later.');
+}
